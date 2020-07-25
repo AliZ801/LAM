4a329ba86b22feb8983b196b7018d59324644468
Ali Zubair <61495779+AliZ801@users.noreply.github.com>
2020-07-25 18:04:29 +0300

    Add project files.

---
+++ LAM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LAM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
